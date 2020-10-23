031b6449d692269ee1710a9949a353c47d6d9b36
SubArtic15 <applekainoa@gmail.com>
2020-10-22 23:44:38 -1000

    reformatted FS && finished setup.sh

---
+++ scripts/splitter.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+if [ "$#" -le 1 ] || [ "$#" -gt 3 ] ; then
+  printf "Invalid Args\n\tfile_name: file to be split\n\tfile_prefix: prefix of new files.\n\tlength: length of split files(default=10000)\n\n"
+
+# if there's only two args then do default line amount
+elif [ "$#" -eq 2 ]; then
+  lines=10000
+
+else
+  let "lines=$3"
+
+fi
+
+if [ "$#" -gt 0 ]; then
+  # setting up variables and such
+  let "num_lines_in_file=$(cat $1 | wc -l)"
+  let "num_output_files=$(( num_lines_in_file / lines + 1 ))"
+  num_suffix=$(echo "l($num_output_files)/l(26)" | bc -l)
+  num_suffix=$(echo $num_suffix | awk '{print int($1+0.5)}')
+  let "num_suffix=num_suffix + 1"
+
+  file_ext=$(echo "$1"|awk -F . '{print $NF}')
+
+
+  rm -rf "$2_split" || return
+  mkdir "$2_split"
+  tail -n +2 $1 | split -l $lines -a $num_suffix - $2_
+  for file in $2_*
+  do
+      if [ "$file" != "$2_split" ]; then
+        head -n 1 $1 > tmp_file
+        cat $file >> tmp_file
+        mv -f tmp_file "$2_split/$file.$file_ext"
+        rm -f $file
+      fi
+  done
+fi
--- setup.sh
+++ setup.sh
@@ -1,35 +1,73 @@
-#!bin/bash
+#!/bin/bash
 
 
 
 # set env variables
-PWD=$(pwd)
+CWD=$(pwd)
+MASTER_FILE=~/.cc_scripts
 SCRIPTS=("splitter.sh" "launch.sh" "container.sh")
 
 
 
 function add_script {
+  # adds the following lines to $MASTER_FILE
+  #
+  # [SCRIPT_NAME]_path=[CWD]/scipts/[SCRIPT_FILE]
+  # alias [SCRIPT_NAME]="source $[SCRIPT_NAME]"
+  #
+
   if [ "$#" -ne 1 ]; then
     echo "[ERROR]: An invalid number of arguments given!"
     return
   fi
 
+  # define local varibales to be loaded later on
   local script_file="$1"
   local script_name=(${script_file//.sh/ })
 
   # information text
   echo "  └──[SETUP]: Adding $script_file..."
 
-  printf "$script_name" >> ~/.bash_profile
-  printf "_path=$PWD/$script_file\n" >> ~/.bash_profile
+  # add line #1 to $MASTER_FILE
+  printf "$script_name" >> $MASTER_FILE
+  printf "_path=$CWD/scripts/$script_file\n" >> $MASTER_FILE
 
-  printf "alias $script_name=" >> ~/.bash_profile
-  printf '"source $' >> ~/.bash_profile
-  printf "$script_name" >> ~/.bash_profile
-  printf "_path" >> ~/.bash_profile
-  printf '"\n' >> ~/.bash_profile
+  # add line #2 to $MASTER_FILE
+  printf "alias $script_name=" >> $MASTER_FILE
+  printf '"source $' >> $MASTER_FILE
+  printf "$script_name" >> $MASTER_FILE
+  printf "_path" >> $MASTER_FILE
+  printf '"\n' >> $MASTER_FILE
 }
 
+
+
+function reset_cc_scripts {
+  echo "[SETUP]: a previous scripts file has been identified."
+  echo "[SETUP]: resetting previous scripts file."
+
+  rm -f $MASTER_FILE
+  touch $MASTER_FILE
+
+  echo "#!/bin/bash\n\n" >> $MASTER_FILE
+}
+
+
+# -------------------------------[ MAIN ]-------------------------------
+# modify ~/.bash_profile only if it has not been modified
+if grep -Fxq "source ~/.cc_scripts" ~/.bash_profile; then
+  echo
+else
+  echo "[SETUP]: modifying ~/.bash_profile to include new scripts"
+  echo "source ~/.cc_scripts" >> ~/.bash_profile
+fi
+
+
+# manage previous compilations
+echo "[SETUP]: checking for existing scripts file..."
+if [[ -f "$MASTER_FILE" ]]; then
+  reset_cc_scripts
+fi
 
 
 # loop through all scripts and add them, and their dependencies to ~/.bash_profile
@@ -38,4 +76,5 @@ for srpt in ${SCRIPTS[@]}; do
   add_script $srpt
 done
 echo -e "[SETUP]: all scripts loaded!\n"
-source ~/.bash_profile
+source $MASTER_FILE
+# -------------------------------[ MAIN ]-------------------------------
